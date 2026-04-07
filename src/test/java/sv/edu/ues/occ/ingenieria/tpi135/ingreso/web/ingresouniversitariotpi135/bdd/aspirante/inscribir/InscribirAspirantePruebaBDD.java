26da87b78043f472684416337b15c63b88116694
Edwin <gm22089@ues.edu.sv>
2026-04-06 22:14:56 -0600

    Test: FLujo completo de inscripcion de aspirante

--- src/test/java/sv/edu/ues/occ/ingenieria/tpi135/ingreso/web/ingresouniversitariotpi135/bdd/aspirante/inscribir/InscribirAspirantePruebaBDD.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi135/ingreso/web/ingresouniversitariotpi135/bdd/aspirante/inscribir/InscribirAspirantePruebaBDD.java
@@ -140,8 +140,8 @@ public class InscribirAspirantePruebaBDD {
         nuevoTurno.setHoraFin(LocalTime.of(11,0));
 
         /**
-         * Resolver llaves foráneas (Relaciones)
-         * Todo turno pertenece a una prueba de admisión específica.
+         * Resolver llaves foráneas
+         * LOs turno pertenece a una prueba de admisión específica.
          * Por lo tanto, instanciamos una prueba y le seteamos nuestra semilla ID_PRUEBA_SEMILLA
          */
         PruebasAdmision pruebasAdmision = new PruebasAdmision();
@@ -167,74 +167,96 @@ public class InscribirAspirantePruebaBDD {
     public void solicito_inscribir_al_aspirante_en_la_prueba_de_admision_y_turno_seleccionados(){
         System.out.println("INSCRIBIENDO al aspirante");
 
+        // CREAR LA INSCRIPCION
         InscripcionesPrueba inscrpciones = new InscripcionesPrueba();
 
+        // Enlazamos al aspirante que creamos en el @Given anterior.
         AspirantesDato nuevoAspirante = new AspirantesDato();
         nuevoAspirante.setId(idAspirante);
         inscrpciones.setIdAspirante(nuevoAspirante);
 
+        // Enlazamos la prueba de admisión
         PruebasAdmision pruebasAdmision = new PruebasAdmision();
         pruebasAdmision.setId(ID_PRUEBA_SEMILLA);
         inscrpciones.setIdPrueba(pruebasAdmision);
 
         inscrpciones.setEstado("INSCRITO");
 
+        // Enviamos la petición POST para guardar la inscripción en la base de datos
         Response respuestaDeInscripcion = hacerPost("inscripciones_prueba", inscrpciones);
         Assertions.assertEquals(201, respuestaDeInscripcion.getStatus());
+
+        // Guardamos el ID generado para usarlo en las validaciones (@Then)
         idInscripcion = extraerIdDelHeader(respuestaDeInscripcion, "inscripciones_prueba");
 
-        //Asociamos una carrera elegida por el usuario a esta inscripcion
+        // ASOCIAR LA CARRERA A LA INSCRIPCIÓN
+
+        // Utilizamos la tabla intermedia
         CarrerasElegidaId carrerasElegida = new CarrerasElegidaId();
         carrerasElegida.setIdInscripcion(idInscripcion);
         carrerasElegida.setIdCarrera(ID_CARRERA_SEMILLA);
 
-        CarrerasElegida  carreras = new CarrerasElegida();
+        // Creamos la entidad principal de la carrera elegida y le asignamos su llave compuesta
+        CarrerasElegida carreras = new CarrerasElegida();
         carreras.setId(carrerasElegida);
         carreras.setPrioridad((short) 1);
-
+cd
+        // Resolvemos la llave foránea hacia el catálogo de carreras
         CatalogoCarrera catalogoCarrera = new CatalogoCarrera();
         catalogoCarrera.setIdCarrera(ID_CARRERA_SEMILLA);
         carreras.setIdCarrera(catalogoCarrera);
 
+        // Enviamos la petición POST para registrar la elección de carrera
         Response respuestaCarrera = hacerPost("carreras_elegidas", carreras);
         Assertions.assertEquals(201, respuestaCarrera.getStatus());
-
     }
 
     @Then("se registra una nueva inscripcion a la prueba para ese aspirante")
     public void se_registra_una_nueva_inscripcion_a_la_prueba_para_ese_aspirante(){
-        System.out.println("Verficamos que se haya creado un ID en la inscripcion");
+        System.out.println("Verificamos que se haya creado un ID en la inscripcion");
+
+        // Si el POST falló, idInscripcion sería nulo.
         Assertions.assertNotNull(idInscripcion,"El ID de la inscripcion no deberia de ser null");
     }
 
     @Then("puedo consultar la inscripcion de ese aspirante a la prueba")
     public void puedo_consultar_la_inscripcion_de_ese_aspirante_a_la_prueba(){
-        System.out.println("COnsultamos la inscripcion generada");
+        System.out.println("Consultamos la inscripcion generada");
 
-        Response respuesta = hacerGet("inscripciones_prueba",idInscripcion);
+        /**
+         * Vamos a la API (simulando un cliente) para verificar que el dato
+         * realmente existe en la base de datos y no fue un falso positivo.
+         */
+        Response respuesta = hacerGet("inscripciones_prueba", idInscripcion);
+
+        // Verificamos que el servidor la encuentre (HTTP 200 OK)
         Assertions.assertEquals(200, respuesta.getStatus());
 
+        // Transformamos el JSON de respuesta de vuelta a un objeto Java
         InscripcionesPrueba inscripcionesConsulatada = respuesta.readEntity(InscripcionesPrueba.class);
-        Assertions.assertEquals(idInscripcion,inscripcionesConsulatada.getId());
-        Assertions.assertEquals(idAspirante,inscripcionesConsulatada.getIdAspirante().getId());
 
+        // Validamos la integridad de los datos:
+        Assertions.assertEquals(idInscripcion, inscripcionesConsulatada.getId());
+        Assertions.assertEquals(idAspirante, inscripcionesConsulatada.getIdAspirante().getId());
     }
 
     @Then("la inscripcion muestra el turno de examen y la carrera elegida correctamente")
     public void la_inscripcion_muestra_el_turno_de_examen_y_la_carrera_elegida_correctamente(){
         System.out.println("La inscripcion muestra el turno de examen Y la carrera elegida por el aspirante");
 
+        // Consultamos el endpoint usando la llave compuesta (ID de la inscripción + ID de la carrera)
         Response respuesta = target.path("carreras_elegidas/{idInscripcion}/{idCarrera}")
-                .resolveTemplate("idInscripcion",idInscripcion)
-                .resolveTemplate("idCarrera",ID_CARRERA_SEMILLA)
+                .resolveTemplate("idInscripcion", idInscripcion)
+                .resolveTemplate("idCarrera", ID_CARRERA_SEMILLA)
                 .request(MediaType.APPLICATION_JSON)
                 .get();
 
+        // Verificamos que la relación se haya guardado y pueda ser consultada (HTTP 200 OK)
         Assertions.assertEquals(200, respuesta.getStatus());
-        CarrerasElegida carreraEncontrada = respuesta.readEntity(CarrerasElegida.class);
-        Assertions.assertEquals(ID_CARRERA_SEMILLA,carreraEncontrada.getId().getIdCarrera());
 
+        // Extraemos la entidad y verificamos que el código de carrera coincida con nuestra semilla
+        CarrerasElegida carreraEncontrada = respuesta.readEntity(CarrerasElegida.class);
+        Assertions.assertEquals(ID_CARRERA_SEMILLA, carreraEncontrada.getId().getIdCarrera());
     }
-    
 
 }
